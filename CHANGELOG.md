77335fea148ecfd5a6ed753854d903d58a517115
Yuriy Pitomets <pitometsu@gmail.com>
2016-06-03 17:10:57 +0300

    Initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change log
+
+funnyprint uses [Semantic Versioning][].
+The change log is available through the [releases on GitHub][].
+
+[Semantic Versioning]: http://semver.org/spec/v2.0.0.html
+[releases on GitHub]: https://github.com/Pitometsu/funnyprint/releases
---
+++ library/FunnyPrint.hs
@@ -0,0 +1,53 @@
+-- | funnyPrint function to colorize GHCi output. See @FunnyPrint.funnyPrint@.
+module FunnyPrint (funnyPrint) where
+
+import IPPrint ( pshow )
+import Language.Haskell.HsColour.Output ( TerminalType(..) )
+import Language.Haskell.HsColour.Colourise ( defaultColourPrefs
+                                           , Colour(..)
+                                           , Highlight(..)
+                                           )
+import Language.Haskell.HsColour ( ColourPrefs(..)
+                                 , Output(TTYg)
+                                 , hscolour
+                                 -- , conid
+                                 -- , conop
+                                 -- , string
+                                 -- , char
+                                 -- , layout
+                                 -- , number
+                                 -- , keyglyph
+                                 )
+
+-- | Colorize GHCi. UTF8 support. Smart indentatntion. Use as @:set -interactive-print=funnyPrint@.
+funnyPrint :: (Show a) => a -> IO ()
+funnyPrint = putStrLn . colorize . con . pshow
+  where
+    con :: String -> String
+    con [] = []
+    con li@(x:xs) | x == '\"' = '\"':str ++ "\"" ++ (con rest)
+                  | x == '\'' = '\'':chr:'\'':(con rest')
+                  | otherwise = x:con xs
+      where
+        (str, rest):_  = reads li :: [(String, String)]
+        (chr, rest'):_ = reads li :: [(Char,   String)]
+    prefs = TTYg XTerm256Compatible
+    colorize = hscolour prefs colours False False "" False
+    colours = defaultColourPrefs { conid    = [ yellow , bold ]
+                                 , conop    = [ yellow ]
+                                 , string   = [ green ]
+                                 , char     = [ cyan ]
+                                 , number   = [ red , bold ]
+                                 , layout   = [ black ]
+                                 , keyglyph = [ black ]
+                                 }
+      where
+        black   = Foreground Black
+        red     = Foreground Red
+        green   = Foreground Green
+        yellow  = Foreground Yellow
+        -- blue    = Foreground Blue
+        magenta = Foreground Magenta
+        cyan    = Foreground Cyan
+        -- white   = Foreground White
+        bold    = Bold
